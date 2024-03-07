c0ac65169ba9fc4c909103236ac2b2aa4cfdde7e
LYin <2113635977@qq.com>
2024-03-07 21:09:43 +0800

    提交本地代码到仓库

---
+++ content/practice0.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include "GradeBook.h"
+using namespace std;
+int main()
+{
+    GradeBook gradeBook1("CS101 Introduction to Programming in C++");
+    GradeBook gradeBook2("CS102 C++ Data Structures");
+    cout<<"gradeBook1's initial course name is: "
+        <<gradeBook1.getCourseName()
+        <<"\ngradeBook2's initial course name is: "
+        <<gradeBook2.getCourseName()<<endl;
+        gradeBook1.setCourseName("CS101 C++ Programming");
+    cout<<"\ngradeBook1's course name is: "
+        <<gradeBook1.getCourseName()
+        <<"\ngradeBook2's course name is: "
+        <<gradeBook2.getCourseName()<<endl;
+}
